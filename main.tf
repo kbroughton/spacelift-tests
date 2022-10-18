1b8200d54a2ed13a96abbd60b198ec474ae7ae10
kesten <kesten.broughton@gmail.com>
2022-10-18 08:53:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     spacelift = {
-      source = "github.com/spacelift-io//terraform-provider-spacelift"
+      source = "github.com/spacelift-io/terraform-provider-spacelift"
       
     }
     aws = {
