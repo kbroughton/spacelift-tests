2c3d54333f9ea2e3b2a30cd56e87e170e4a38be2
kesten <kesten.broughton@gmail.com>
2022-10-18 09:44:04 -0500

    version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     spacelift = {
       source = "spacelift-io/spacelift"
-      
+      version = "~> 0.1.30"
     }
     aws = {
       source  = "hashicorp/aws"
